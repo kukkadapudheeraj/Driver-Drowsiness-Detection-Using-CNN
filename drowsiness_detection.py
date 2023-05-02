621e00bcf15b9e27c9f7074e6a865595bbada9fb
kukkadapudheeraj <kukkadapudheeraj@gmail.com>
2023-05-02 12:24:13 -0400

    Update drowsiness_detection.py

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -1,1 +1,155 @@
+!ls /kaggle/input/drowsiness-detection
+
+
+import cv2, os
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+%matplotlib inline
+from matplotlib.image import imread
+import tensorflow as tf 
+from tensorflow import keras
+from tensorflow.keras import models, layers, Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.applications import ResNet50
+from keras.applications.resnet50 import preprocess_input
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+
+data_dir = '/kaggle/input/drowsiness-detection'
+
+
+
+
+
+train_datagen = ImageDataGenerator(validation_split=0.2,
+                                   preprocessing_function=preprocess_input) # don't use rescale = 1./255
+
+train_generator = train_datagen.flow_from_directory( data_dir,
+                                                     target_size=(224,224),
+                                                     batch_size=100,
+                                                     shuffle=True,
+                                                     class_mode='categorical',
+                                                     subset='training')
+
+validation_datagen = ImageDataGenerator(validation_split=0.2,
+                                        preprocessing_function=preprocess_input)
+
+validation_generator =  validation_datagen.flow_from_directory( data_dir,
+                                                                target_size=(224,224),
+                                                                batch_size=100,
+                                                                class_mode='categorical',
+                                                                subset='validation')
+
+
+
+
+
+
+
+
+
+
+
+
+model_res50 = Sequential()
+
+model_res50.add(ResNet50(
+    include_top=False,
+    pooling='avg',
+    weights='imagenet'
+    ))
+
+model_res50.add(Dense(2, activation='softmax'))
+
+model_res50.layers[0].trainable = False 
+
+model_res50.summary()
+
+
+steps_per_epoch_training = len(train_generator)
+steps_per_epoch_validation = len(validation_generator)
+
+
+
+
+
+model_res50.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+history = model_res50.fit(
+    train_generator,
+    steps_per_epoch=steps_per_epoch_training,
+    validation_steps=steps_per_epoch_validation,
+    epochs=7,
+    validation_data=validation_generator,
+    verbose=1
+)
+
+
+
+
+
+%matplotlib inline
+
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+
+
+
+
+
+
+
+model_res50.save('drowsines_model_1_ResNet50_Binary2.h5')
+
+
+
+
+
+
+
+img_array = cv2.imread('../input/drowsiness-detection/open_eye/s0001_01844_0_0_1_0_0_01.png', cv2.IMREAD_GRAYSCALE)
+backtorgb = cv2.cvtColor(img_array, cv2.COLOR_GRAY2BGR)
+new_array = cv2.resize(backtorgb, (224, 224))
+X_input = np.array(new_array).reshape(1, 224, 224, 3)
+# plt.imshow(new_array)
+prediction = model_res50.predict(X_input/255.0)
+prediction
+
 
