b1dc239d767994e1ea5f8c1257f52a4e5b97c747
gogineni1998 <39270552+gogineni1998@users.noreply.github.com>
2023-05-14 18:45:05 -0400

    Update drowsiness_detection.py

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -25,24 +25,19 @@ data_dir = '/kaggle/input/drowsiness-detection'
 
 
 
-train_datagen = ImageDataGenerator(validation_split=0.2,
-                                   preprocessing_function=preprocess_input) # don't use rescale = 1./255
+# Define an instance of ImageDataGenerator for training data with a validation split of 20% and preprocess the input images using preprocess_input
+train_datagen = ImageDataGenerator(validation_split=0.2, preprocessing_function=preprocess_input)
 
-train_generator = train_datagen.flow_from_directory( data_dir,
-                                                     target_size=(224,224),
-                                                     batch_size=100,
-                                                     shuffle=True,
-                                                     class_mode='categorical',
-                                                     subset='training')
+# Define a generator for training data by calling flow_from_directory method on train_datagen
+# flow_from_directory generates batches of augmented and preprocessed images from the specified directory
+train_generator = train_datagen.flow_from_directory(data_dir, target_size=(224,224), batch_size=100, shuffle=True, class_mode='categorical', subset='training')
 
-validation_datagen = ImageDataGenerator(validation_split=0.2,
-                                        preprocessing_function=preprocess_input)
+# Define an instance of ImageDataGenerator for validation data with a validation split of 20% and preprocess the input images using preprocess_input
+validation_datagen = ImageDataGenerator(validation_split=0.2, preprocessing_function=preprocess_input)
 
-validation_generator =  validation_datagen.flow_from_directory( data_dir,
-                                                                target_size=(224,224),
-                                                                batch_size=100,
-                                                                class_mode='categorical',
-                                                                subset='validation')
+# Define a generator for validation data by calling flow_from_directory method on validation_datagen
+# flow_from_directory generates batches of preprocessed images from the specified directory
+validation_generator = validation_datagen.flow_from_directory(data_dir, target_size=(224,224), batch_size=100, class_mode='categorical', subset='validation')
 
 
 
@@ -54,22 +49,26 @@ validation_generator =  validation_datagen.flow_from_directory( data_dir,
 
 
 
-
+# Create a Sequential model instance
 model_res50 = Sequential()
 
+# Add a ResNet50 layer to the model with some specific configuration
 model_res50.add(ResNet50(
     include_top=False,
     pooling='avg',
     weights='imagenet'
     ))
 
+# Add a Dense layer with 2 output nodes and a softmax activation function
 model_res50.add(Dense(2, activation='softmax'))
 
+# Set the ResNet50 layer to be non-trainable
 model_res50.layers[0].trainable = False 
 
+# Print a summary of the model's architecture
 model_res50.summary()
 
-
+# Determine the number of steps per epoch for training and validation generators
 steps_per_epoch_training = len(train_generator)
 steps_per_epoch_validation = len(validation_generator)
 
@@ -77,20 +76,24 @@ steps_per_epoch_validation = len(validation_generator)
 
 
 
-model_res50.compile(optimizer='adam', 
-              loss='categorical_crossentropy', 
-              metrics=['accuracy'])
 
+# Compile the model with an Adam optimizer, categorical cross-entropy loss, and accuracy metric
+model_res50.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model using the fit() method and the training and validation generators.
+# Set the number of steps per epoch for training and validation, the number of epochs to train for,
+# the validation data to use, and the verbosity level (1 for progress bar).
 history = model_res50.fit(
     train_generator,
     steps_per_epoch=steps_per_epoch_training,
     validation_steps=steps_per_epoch_validation,
-    epochs=7,
+    epochs=3,
     validation_data=validation_generator,
     verbose=1
 )
 
 
+# This line saves the model in 'drowsines_model_1_ResNet50_Binary2.h5' file
 model_res50.save('drowsines_model_1_ResNet50_Binary2.h5')
 
 
@@ -99,13 +102,26 @@ model_res50.save('drowsines_model_1_ResNet50_Binary2.h5')
 
 
 
+# Load the image file in grayscale using OpenCV
 img_array = cv2.imread('../input/drowsiness-detection/open_eye/s0001_01844_0_0_1_0_0_01.png', cv2.IMREAD_GRAYSCALE)
+
+# Convert the grayscale image to a 3-channel RGB image
 backtorgb = cv2.cvtColor(img_array, cv2.COLOR_GRAY2BGR)
+
+# Resize the image to the required input shape of the model (224x224) using OpenCV
 new_array = cv2.resize(backtorgb, (224, 224))
+
+# Convert the image to a 4D numpy array with shape (1, 224, 224, 3) to match the input shape of the model
 X_input = np.array(new_array).reshape(1, 224, 224, 3)
-# plt.imshow(new_array)
-prediction = model_res50.predict(X_input/255.0)
-prediction
+
+# Normalize the input image data by scaling pixel values to be between 0 and 1
+X_input = X_input / 255.0
+
+# Use the trained model to predict the class probabilities of the input image
+prediction = model_res50.predict(X_input)
+
+# Print the predicted class probabilities
+print(prediction)
 
 
 
